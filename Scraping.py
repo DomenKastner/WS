e2050bb4fdcc19537b2900747bad095fe5f56d18
DomenKastner <domen.kastner@gmail.com>
2018-02-04 20:23:51 +0100

    completed exercise

--- Scraping.py
+++ Scraping.py
@@ -5,10 +5,10 @@ from BeautifulSoup import BeautifulSoup
 
 
 class Oseba:   # objekt Oseba
-    def __init__(self, name, surname, city, email):
+    def __init__(self, name, surname, age, city, email):
         self.name = name
         self.surname = surname
-        #self.age = age
+        self.age = age
         self.city = city
         self.email = email
 
@@ -18,43 +18,50 @@ MAIN_URL = "https://scrapebook22.appspot.com/"
 # MAIN_URL odpremo z url.open in ga preberemo z .read(). To damo v spremenljivko webpage
 webpage = urlopen(MAIN_URL).read()
 
-""" 
-  Odprto in prebrano webpage sedaj pošljemo skozi BeautifulSoup za lažje skrejpanje in to 
-  damo v spremenljivko nice_webpage. 
-"""
+# Odprto in prebrano webpage sedaj pošljemo skozi BeautifulSoup za lažje skrejpanje in to
+# damo v spremenljivko nice_webpage.
+
 nice_webpage = BeautifulSoup(webpage)
 print(nice_webpage.html.head.title.string)
 
+persons_data = []  #seznam v katerega se shranijo podatke
 
-persons_data = []  #seznam v katerega vstavimo podatke
-
-
-
-linki = nice_webpage.findAll("a")
+linki = nice_webpage.findAll("a")  # poišče vse "a" tage v nice_webpage
 for link in linki:
-    if link.string == "See full profile":
+    if link.string == "See full profile":  # Če je link z stringom "See full profile"
+    # Odpremo in preberemo povezavo vsakega profila posebaj in to damo v spremenljivko personal_page
         personal_page = BeautifulSoup(urlopen(MAIN_URL + link["href"]).read())
 
+    # Ker sta na strani 2 "h1" in je ime in priimek drugi izberemo tega z [-1]
         personal_name = personal_page.findAll("h1")[-1].string
         name, surname = personal_name.split(" ")
         print (surname + " - " + name)
 
-        p_city = personal_page.find("span", {"data-city"}).string
-        city = p_city
-        print (p_city)
+    #Ker imamo več "li" moramo izbrati .findAll in z [x]izbrati njegov položaj
+        persons_age = personal_page.findAll("li")[1].string
+        print (persons_age)
 
-        p_email = personal_page.find("span", {"class": "email"}).string
-        email = p_email
-        print (p_email)
+    # V sprem. persons_city shranimo pot do vrednost za izpis. Ker ima data-city različne vrednosti
+    # damo za ispis te value True.
+        persons_city = personal_page.find("span", {"data-city": True}).string
+        print (persons_city)
 
-        person = Oseba(name, surname, p_city, p_email)
-        persons_data.append(person)
+    # V sprem. personal_email shranimo pot do vrednost za izpis
+        personal_email = personal_page.find("span", {"class": "email"}).string
+        print (personal_email)
 
-print(persons_data)
+    #V sprem. person damo vse do sedaj zbrane podatke
+        person = Oseba(name, surname, persons_age, persons_city, personal_email)
+        persons_data.append(person)  # V list person_data damo vse vrednosti k jih imamo v  spre. person
 
-csv_file = open("persons_data.csv", "w")
 
-for person in persons_data:
-    csv_file.write(person.name + "," + person.surname + "," + person.city + "," + person.email + "\n")
+print(persons_data)  # print v interpreter
 
+csv_file = open("persons_data.csv", "w")  # Ustvari in odpre CSV dat.
+
+for person in persons_data:  # Podatki osebe person v seznamu person_data
+    # Spodaj vpišemo v kakem vrstnem redu želimo izpis podatkov v CSV dat.
+    csv_file.write(person.name + "," + person.surname + "," + person.age + "," + person.city + "," + person.email + "\n")
+
+    # Vedno na koncu zapri CSV file
 csv_file.close()
